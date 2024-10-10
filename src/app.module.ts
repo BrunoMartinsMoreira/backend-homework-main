1d8d6346878bc2316bb04422fae8fb6df0ee7dbf
Bruno Moreira <bruno.moreira@cargon.com.br>
2024-10-09 21:42:23 -0300

    feat: update get-user middleware to throw error if user is not found

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './configs/ormconfig';
 import { UserMiddleware } from './get-user-middleware';
 import { ProposalsModule } from './core/proposals/proposals.module';
+import { UsersModule } from './core/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), ProposalsModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    ProposalsModule,
+    UsersModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/get-user-middleware.ts
+++ src/get-user-middleware.ts
@@ -1,4 +1,8 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { AbstractUsersRepository } from './core/users/repositories';
 
@@ -9,10 +13,12 @@ export class UserMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     const userId = req.headers['user_id'];
     if (userId) {
-      const user = await this.userRepository.findOne({ id: Number(userId) });
-      if (user) {
-        req.user = user;
-      }
+      const user = await this.userRepository.findOne({
+        id: Number(userId),
+      });
+
+      if (!user) throw new UnauthorizedException('User not found');
+      req.user = user;
     }
     next();
   }
